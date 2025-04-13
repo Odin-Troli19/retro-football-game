42d880224717f003c6e71fb7087987b30eaa2d60
odintroli19 <sturetroly1@gmail.com>
2025-04-13 21:29:13 +0200

    adding initial files

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import BootScene from './scenes/BootScene.js';
+import PreloadScene from './scenes/PreloadScene.js';
+import MenuScene from './scenes/MenuScene.js';
+import GameScene from './scenes/GameScene.js';
+import UIScene from './scenes/UIScene.js';
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 480,
+    parent: 'game-container',
+    pixelArt: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
+    scene: [
+        BootScene,
+        PreloadScene,
+        MenuScene,
+        GameScene,
+        UIScene
+    ]
+};
+
+const game = new Phaser.Game(config);
